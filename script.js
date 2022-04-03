65cfcf164bb7e64f79bd747aa98d42905c2d32d5
Ge0h <george.walker.wme@gmail.com>
2022-04-03 14:09:15 +0100

    Re-factor: Remove test console logs

--- script.js
+++ script.js
@@ -1,72 +1,69 @@
-const apiKey = 'dc5ccd32a8df91c843db8202bfff0abe';
+const apiKey = "dc5ccd32a8df91c843db8202bfff0abe";
 
-const weatherInfo = document.createElement('img')
-const tempInfo = document.createElement('div')
+const weatherInfo = document.createElement("img");
+const tempInfo = document.createElement("div");
 
-const locationHeader = document.createElement('div')
-const locationHeaderContainer = document.createElement('div')
-locationHeaderContainer.classList.add('header')
-locationHeader.classList.add('flex')
+const locationHeader = document.createElement("div");
+const locationHeaderContainer = document.createElement("div");
+locationHeaderContainer.classList.add("header");
+locationHeader.classList.add("flex");
 
+const locationInput = document.createElement("input");
+locationInput.placeholder = "Location";
+let locationValue = "London";
 
-const locationInput = document.createElement('input')
-locationInput.placeholder = 'Location'
-let locationValue = 'London'
+const currentLocation = document.createElement("h1");
+currentLocation.textContent = locationValue;
 
-const currentLocation = document.createElement('h1')
-currentLocation.textContent = locationValue
+const form = document.createElement("form");
 
-const form = document.createElement('form')
-
-const button = document.createElement('button')
-button.textContent = 'Go'
-button.classList.add('btn')
+const button = document.createElement("button");
+button.textContent = "Go";
+button.classList.add("btn");
 
 const celciusConverter = (kelvin) => {
-    return Math.round(kelvin - 273.15)
-}
+  return Math.round(kelvin - 273.15);
+};
 
-// const locationContent = (input) => {
-//     console.log('click')
-//     locationValue = `${input}`
-// }
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locationValue}&APPID=dc5ccd32a8df91c843db8202bfff0abe`, {mode: 'cors'})
-  .then(function(response) {
-    return response.json()
+fetch(
+  `http://api.openweathermap.org/data/2.5/weather?q=${locationValue}&APPID=${apiKey}`,
+  { mode: "cors" }
+)
+  .then(function (response) {
+    return response.json();
   })
-  .then(function(response) {
-    console.log(response)
+  .then(function (response) {
     weatherInfo.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-    tempInfo.textContent = `${celciusConverter(response.main.temp)} °C`
-  })
+    tempInfo.textContent = `${celciusConverter(response.main.temp)} °C`;
+  });
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    locationValue = `${locationInput.value}`
-    currentLocation.textContent = locationValue
-    locationInput.value = ''
-    console.log(locationValue)
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locationValue}&APPID=dc5ccd32a8df91c843db8202bfff0abe`, {mode: 'cors'})
-      .then(function(response) {
-        return response.json()
-      })
-      .then(function(response) {
-        console.log(response)
-        weatherInfo.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
-        tempInfo.textContent = `${celciusConverter(response.main.temp)} °C`
-      })
-      .catch(function(err) {
-        alert('Enter City Name')
-      })
-}) 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  locationValue = `${locationInput.value}`;
+  currentLocation.textContent = locationValue;
+  locationInput.value = "";
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${locationValue}&APPID=${apiKey}`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      weatherInfo.src = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+      tempInfo.textContent = `${celciusConverter(response.main.temp)} °C`;
+    })
+    .catch(function (err) {
+      alert("Enter Town or City Name");
+    });
+});
 
-const content = document.querySelector('#content')
-content.append(locationHeaderContainer)
-locationHeaderContainer.append(locationHeader)
-locationHeader.append(form)
-form.append(locationInput)
-form.append(button)
-locationHeader.append(currentLocation)
-content.append(weatherInfo)
-content.append(tempInfo)
+const content = document.querySelector("#content");
+content.append(locationHeaderContainer);
+locationHeaderContainer.append(locationHeader);
+locationHeader.append(form);
+form.append(locationInput);
+form.append(button);
+locationHeader.append(currentLocation);
+content.append(weatherInfo);
+content.append(tempInfo);
